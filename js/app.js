9c6724792e2adb4e9a4261894eee6cb4a8d19155
kevinmcalear <kevin.mcalear@gmail.com>
2015-03-01 02:21:55 -0500

    added in better main page design

--- js/app.js
+++ js/app.js
@@ -3,49 +3,185 @@
 /* @params: set the params [object, optional, default values : check particles.js] */
 
 /* config dom id (optional) + config particles params */
-particlesJS('particles-js', {
-  particles: {
-    color: '#fff',
-    shape: 'circle', // "circle", "edge" or "triangle"
-    opacity: 1,
-    size: 4,
-    size_random: true,
-    nb: 150,
-    line_linked: {
-      enable_auto: true,
-      distance: 100,
+
+
+// particlesJS('particles-js', {
+//   particles: {
+//     color: '#fff',
+//     shape: 'circle', // "circle", "edge" or "triangle"
+//     opacity: 1,
+//     size: 4,
+//     size_random: true,
+//     nb: 150,
+//     line_linked: {
+//       enable_auto: true,
+//       distance: 100,
+//       color: '#fff',
+//       opacity: 1,
+//       width: 1,
+//       condensed_mode: {
+//         enable: false,
+//         rotateX: 600,
+//         rotateY: 600
+//       }
+//     },
+//     anim: {
+//       enable: true,
+//       speed: 1
+//     }
+//   },
+//   interactivity: {
+//     enable: true,
+//     mouse: {
+//       distance: 250
+//     },
+//     detect_on: 'canvas', // "canvas" or "window"
+//     mode: 'grab',
+//     line_linked: {
+//       opacity: .5
+//     },
+//     events: {
+//       onclick: {
+//         enable: true,
+//         mode: 'push', // "push" or "remove" (particles)
+//         nb: 4
+//       }
+//     }
+//   },
+//   /* Retina Display Support */
+//   retina_detect: true
+// });
+
+
+/* DESKTOP */
+
+function pJS_desktop(){
+  particlesJS('particles-js', {
+    particles: {
       color: '#fff',
-      opacity: 1,
-      width: 1,
-      condensed_mode: {
-        enable: false,
-        rotateX: 600,
-        rotateY: 600
-      }
-    },
-    anim: {
-      enable: true,
-      speed: 1
-    }
-  },
-  interactivity: {
-    enable: true,
-    mouse: {
-      distance: 250
-    },
-    detect_on: 'canvas', // "canvas" or "window"
-    mode: 'grab',
-    line_linked: {
-      opacity: .5
-    },
-    events: {
-      onclick: {
+      shape: 'circle',
+      opacity: .2,
+      size: 2.5,
+      size_random: true,
+      nb: 150,
+      line_linked: {
+        enable_auto: true,
+        distance: 250,
+        color: '#fff',
+        opacity: 0.3,
+        width: 1,
+        condensed_mode: {
+            enable: false,
+            rotateX: 600,
+            rotateY: 600
+        }
+      },
+      anim: {
         enable: true,
-        mode: 'push', // "push" or "remove" (particles)
-        nb: 4
+        speed: 3
       }
+    },
+    interactivity: {
+      enable: true,
+      mouse: {
+        distance: 200
+      },
+      detect_on: 'canvas',
+      mode: 'grab',
+      line_linked: {
+        opacity: .5
+      },
+      events: {
+        onclick: {
+          enable: true,
+          mode: 'push',
+          nb: 4
+        }
+      }
+    },
+    retina_detect: true
+  });
+}
+
+
+
+/* MOBILE / TABLET */
+
+function pJS_mobile(){
+  particlesJS('particles-js', {
+    particles: {
+      color: '#fff',
+      shape: 'circle',
+      opacity: 1,
+      size: 2.5,
+      size_random: true,
+      nb: 40,
+      line_linked: {
+        enable_auto: false,
+        distance: 250,
+        color: '#fff',
+        opacity: 0.5,
+        width: 1,
+        condensed_mode: {
+            enable: false,
+            rotateX: 600,
+            rotateY: 600
+        }
+      },
+      anim: {
+        enable: true,
+        speed: 3
+      }
+    },
+    interactivity: {
+      enable: false,
+      mouse: {
+        distance: 200
+      },
+      detect_on: 'canvas',
+      mode: 'grab',
+      line_linked: {
+        opacity: .5
+      },
+      events: {
+        onclick: {
+          enable: true,
+          mode: 'push',
+          nb: 4
+        }
+      }
+    },
+    retina_detect: true
+  });
+}
+
+
+/* LAUNCH */
+
+if(window.innerWidth > 1100){
+  pJS_desktop();
+}else{
+  pJS_mobile();
+}
+
+/* on resize */
+
+window.addEventListener('resize', function() {
+  checkOnResize();
+}, true);
+
+function checkOnResize(){
+  if(window.innerWidth > 1100){
+    if(pJS.particles.nb != 150){
+      console.log('desktop mode')
+      pJS.fn.vendors.destroy();
+      pJS_desktop();
     }
-  },
-  /* Retina Display Support */
-  retina_detect: true
-});
+  }else{
+    if(pJS.particles.nb == 150){
+      console.log('mobile mode');
+      pJS.fn.vendors.destroy();
+      pJS_mobile();
+    }
+  }
+}
