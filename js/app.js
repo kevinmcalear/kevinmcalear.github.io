c023f7290638e6d68d5e20af2fc19a77547957dc
kevinmcalear <kevin.mcalear@gmail.com>
2015-03-04 00:51:34 -0500

    finished main animation

--- js/app.js
+++ js/app.js
@@ -55,9 +55,257 @@ function pJS_desktop(){
   });
 }
 
+
+function pJS_dev(){
+  particlesJS('particles-js', {
+    particles: {
+      color: '#FFF',
+      shape: 'triangle',
+      opacity: 0.75,
+      size: 4,
+      size_random: true,
+      nb: 150,
+      line_linked: {
+        enable_auto: true,
+        distance: 250,
+        color: '#fff',
+        opacity: 0.75,
+        width: 0.3,
+        condensed_mode: {
+            enable: false,
+            rotateX: 600,
+            rotateY: 600
+        }
+      },
+      anim: {
+        enable: true,
+        speed: 3
+      }
+    },
+    interactivity: {
+      enable: true,
+      mouse: {
+        distance: 250
+      },
+      detect_on: 'canvas',
+      mode: 'grab',
+      line_linked: {
+        color:'#FF0000',
+        opacity: 0.25
+      },
+      events: {
+        onclick: {
+          enable: true,
+          mode: 'push',
+          nb: 4
+        }
+      }
+    },
+    retina_detect: true
+  });
+}
+
+
+
+function pJS_design(){
+  particlesJS('particles-js', {
+    particles: {
+      color: '#30223C',
+      shape: 'triangle',
+      opacity: 0.7,
+      size: 18.5,
+      size_random: true,
+      nb: 150,
+      line_linked: {
+        enable_auto: true,
+        distance: 250,
+        color: '#fff',
+        opacity: 0.25,
+        width: 30.3,
+        condensed_mode: {
+            enable: false,
+            rotateX: 600,
+            rotateY: 600
+        }
+      },
+      anim: {
+        enable: true,
+        speed: 3
+      }
+    },
+    interactivity: {
+      enable: true,
+      mouse: {
+        distance: 250
+      },
+      detect_on: 'canvas',
+      mode: 'grab',
+      line_linked: {
+        color:'#FF0000',
+        opacity: 0.25
+      },
+      events: {
+        onclick: {
+          enable: true,
+          mode: 'push',
+          nb: 4
+        }
+      }
+    },
+    retina_detect: true
+  });
+}
+
+
+function pJS_data(){
+  particlesJS('particles-js', {
+    particles: {
+      color: '#000000',
+      shape: 'edge',
+      opacity: 0.75,
+      size: 5.5,
+      size_random: true,
+      nb: 150,
+      line_linked: {
+        enable_auto: true,
+        distance: 250,
+        color: '#fff',
+        opacity: 0.75,
+        width: 0.3,
+        condensed_mode: {
+            enable: false,
+            rotateX: 600,
+            rotateY: 600
+        }
+      },
+      anim: {
+        enable: true,
+        speed: 3
+      }
+    },
+    interactivity: {
+      enable: true,
+      mouse: {
+        distance: 250
+      },
+      detect_on: 'canvas',
+      mode: 'grab',
+      line_linked: {
+        color:'#FF0000',
+        opacity: 0.6
+      },
+      events: {
+        onclick: {
+          enable: true,
+          mode: 'push',
+          nb: 4
+        }
+      }
+    },
+    retina_detect: true
+  });
+}
+
+
 /* MOBILE / TABLET */
 
 function pJS_mobile(){
+  particlesJS('particles-js', {
+    particles: {
+      color: '#fff',
+      shape: 'circle',
+      opacity: 1,
+      size: 2.5,
+      size_random: true,
+      nb: 40,
+      line_linked: {
+        enable_auto: false,
+        distance: 250,
+        color: '#fff',
+        opacity: 0.5,
+        width: 1,
+        condensed_mode: {
+            enable: false,
+            rotateX: 600,
+            rotateY: 600
+        }
+      },
+      anim: {
+        enable: true,
+        speed: 3
+      }
+    },
+    interactivity: {
+      enable: false,
+      mouse: {
+        distance: 200
+      },
+      detect_on: 'canvas',
+      mode: 'grab',
+      line_linked: {
+        opacity: .5
+      },
+      events: {
+        onclick: {
+          enable: true,
+          mode: 'push',
+          nb: 4
+        }
+      }
+    },
+    retina_detect: true
+  });
+}
+
+function pJS_edge(){
+  particlesJS('particles-js', {
+    particles: {
+      color: '#fff',
+      shape: 'edge',
+      opacity: 1,
+      size: 2.5,
+      size_random: true,
+      nb: 40,
+      line_linked: {
+        enable_auto: false,
+        distance: 250,
+        color: '#fff',
+        opacity: 0.5,
+        width: 1,
+        condensed_mode: {
+            enable: false,
+            rotateX: 600,
+            rotateY: 600
+        }
+      },
+      anim: {
+        enable: true,
+        speed: 3
+      }
+    },
+    interactivity: {
+      enable: false,
+      mouse: {
+        distance: 200
+      },
+      detect_on: 'canvas',
+      mode: 'grab',
+      line_linked: {
+        opacity: .5
+      },
+      events: {
+        onclick: {
+          enable: true,
+          mode: 'push',
+          nb: 4
+        }
+      }
+    },
+    retina_detect: true
+  });
+}
+
+function pJS_tri(){
   particlesJS('particles-js', {
     particles: {
       color: '#fff',
@@ -146,34 +394,44 @@ theater
 
 theater
   .write("Kevin:Hello.")
-  .write(400)
+  .write(500)
   .write("Kevin:My Name is Kevin.")
   .write(500)
-  .write("Kevin:I am", 600, developer, " a developer.", 800)
-  .write(-10)
-  .write(designer, " designer.", 800)
-  .write(-10)
-  .write(dataScientist, " data scientist.", 800)
-  .write(intro,"Kevin:Welcome to my site.", 1600)
-  .write("Kevin:Explore away.", 1000, addButton);
+  .write("Kevin:I love", 600, developer, " programming.", 1000)
+  .write(-13)
+  .write(designer, " design.", 1000)
+  .write(-8)
+  .write(dataScientist, " data science.", 1000)
+  // .write("Kevin:and a lot of other things...", 800)
+  .write(intro,"Kevin:Welcome to my site.", 1000, addButton);
+  // .write("Kevin:Explore away.", 1000, addButton);
   // .write(function () { theater.play(true); });
 
 
 
 function designer () {
   document.getElementById('particles-js').className = "designer";
+  // pJS.fn.vendors.destroy();
+  // // pJS_design();
+  // pJS_mobile();
 }
 
 function developer () {
   document.getElementById('particles-js').className = "developer";
+  // pJS.fn.vendors.destroy();
+  // pJS_dev();
 }
 
 function dataScientist () {
   document.getElementById('particles-js').className = "data-scientist";
+  // pJS.fn.vendors.destroy();
+  // pJS_data();
 }
 
 function intro () {
   document.getElementById('particles-js').className = "intro";
+  // pJS.fn.vendors.destroy();
+  // pJS_desktop();
 }
 
 function explore () {
