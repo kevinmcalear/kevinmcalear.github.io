6d768b836e56ec14566c18a30a1bb3881b315dec
kevinmcalear <kevin.mcalear@gmail.com>
2015-03-05 02:15:40 -0500

    cleaned out cruft and adding in music

---
+++ js/music.js
@@ -0,0 +1,28 @@
+function loadXMLDoc() {
+    var xmlhttp;
+
+    if (window.XMLHttpRequest) {
+        // code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp = new XMLHttpRequest();
+    } else {
+        // code for IE6, IE5
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 ) {
+           if(xmlhttp.status == 200){
+               document.getElementById("music").innerHTML = xmlhttp.responseText;
+           }
+           else if(xmlhttp.status == 400) {
+              alert('There was an error 400')
+           }
+           else {
+               alert('something else other than 200 was returned')
+           }
+        }
+    }
+
+    xmlhttp.open("GET", "http://hypem.com/playlist/loved/kevinmcalear/json/1/data.js", true);
+    xmlhttp.send();
+}
