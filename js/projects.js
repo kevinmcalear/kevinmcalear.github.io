dab6c4a244b0a1bb6766359102bc159ea9e20757
kevinmcalear <kevin.mcalear@gmail.com>
2015-04-05 20:36:12 -0400

    improved project styling

--- js/projects.js
+++ js/projects.js
@@ -1,12 +1,12 @@
 // Scroll Reveal stuff for songs
-// window.sr = new scrollReveal();
+window.sr = new scrollReveal();
 
-// sorting projects
-var container = document.querySelector('#projects');
-var iso = new Isotope( container, {
-  // options...
-  itemSelector: '.test',
-  masonry: {
-    columnWidth: 200
-  }
-});
+// // sorting projects
+// var container = document.querySelector('#projects');
+// var iso = new Isotope( container, {
+//   // options...
+//   itemSelector: '.test',
+//   masonry: {
+//     columnWidth: 200
+//   }
+// });
