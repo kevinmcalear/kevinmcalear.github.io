d3e63521ec75a18375b3b7a5f11ce3c303fed095
kevinmcalear <kevin.mcalear@gmail.com>
2015-04-18 14:14:58 -0400

    figuring out project view

--- js/projects.js
+++ js/projects.js
@@ -1,6 +1,43 @@
 // Scroll Reveal stuff for songs
 window.sr = new scrollReveal();
 
+
+var wall = $('#projects').GITheWall({
+  // Callbacks API
+  onBeforeInit: null,
+  onReady: null,
+  onViewPortUpdate: null,
+  onItemChange: null,
+  onDestroy: null,
+  onShow: null,
+  onHide: null,
+  onContentLoading: null,
+  onContentLoaded: null,
+  margin: {
+    top: 10,
+    bottom: 10
+  },
+  scrollerElm: null,
+  scrollOffset: 150,
+  // settings
+  arrows: true,
+  closebutton: true,
+  keyboardNavigation: true,
+  animationSpeed: 300,
+  autoscroll: true,
+  responsive: true,
+  initialWrapperHeight: 600,
+  dynamicHeight: true,
+  nextButtonClass: '',
+  prevButtonClass: '',
+  closeButtonClass: ''
+});
+
+
+
+
+
+
 // // sorting projects
 // var container = document.querySelector('#projects');
 // var iso = new Isotope( container, {
@@ -10,3 +47,4 @@ window.sr = new scrollReveal();
 //     columnWidth: 200
 //   }
 // });
+
